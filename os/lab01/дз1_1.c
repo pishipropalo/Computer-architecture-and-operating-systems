fef40e7084c529cf36a221b0f8729e6dad492558
Виктория Бурыкина <vika4268@mail.ru>
2024-05-08 22:27:42 +0300

    add hw1

---
+++ os/lab01/дз1_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void swap(int *x, int *y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+int main() {
+    int x, y;
+    scanf("%d %d", &x, &y);
+    swap(&x, &y);
+    printf("%d %d", x, y);
+
+    return 0;
+}
